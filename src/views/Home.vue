3367c068d76d967ae83ba711d2c34658edae887e
Daley <daley.mottley@hotmail.com>
2018-11-14 18:11:59 +0000

    Edit Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Hello!</h1>
+    <p>What you want to do?</p>
+    <router-link to="/auth">Sign in</router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style scoped>
+  
+</style>
