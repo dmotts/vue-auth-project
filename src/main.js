e30b2c18be87a354b47f5a7fb65e360f9f994b96
Daley <daley.mottley@hotmail.com>
2018-11-15 03:52:24 +0000

    Add authentication init to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
+import auth from '@/auth'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
@@ -9,5 +10,8 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  beforeCreate() {
+    auth.init(this)
+  },
   render: h => h(App)
 }).$mount('#app')
